93ff6e2524cb296940f590f0438481a52f696610
Matt Craddock <5796417+craddm@users.noreply.github.com>
2026-01-14 15:17:57 +0000

    create app folder

---
+++ argo-tes-translator/tes2argo/app/main.py
@@ -0,0 +1,25 @@
+import json
+
+
+def parse_tes_task(tes_task_json):
+    """
+    Parses a TES task JSON and extracts relevant information.
+
+    Args:
+        tes_task_json (str): A JSON string representing the TES task.
+    Returns:
+        dict: A dictionary containing extracted information.
+    """
+    tes_task = json.loads(tes_task_json)
+
+    task_info = {
+        "name": tes_task.get("name"),
+        "description": tes_task.get("description"),
+        "tags": tes_task.get("tags", []),
+        "inputs": tes_task.get("inputs", []),
+        "outputs": tes_task.get("outputs", []),
+        "resources": tes_task.get("resources", {}),
+        "executors": tes_task.get("executors", []),
+    }
+
+    return task_info
--- argo-tes-translator/tes2argo/main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from tes2argo!")
-
-
-if __name__ == "__main__":
-    main()
