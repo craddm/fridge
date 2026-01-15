53e15daa102e8236b8e57ac80659185d0ec5ca62
Matt Craddock <5796417+craddm@users.noreply.github.com>
2026-01-15 15:20:19 +0000

    Add initial template for converting to an argo workflow

--- argo-tes-translator/tes2argo/src/main.py
+++ argo-tes-translator/tes2argo/src/main.py
@@ -31,3 +31,42 @@ def parse_tes_task(tes_task_json):
     )
 
     return task
+
+
+def convert_tes_to_argo(task=TesTask):
+    """
+    Converts a TES task JSON to an Argo workflow representation.
+
+    Args:
+        tes_task_json (str): A JSON string representing the TES task.
+    Returns:
+        dict: A dictionary representing the Argo workflow.
+    """
+    task.name
+
+    argo_workflow = {
+        "apiVersion": "argoproj.io/v1alpha1",
+        "kind": "Workflow",
+        "metadata": {"name": task.name},
+        "spec": {
+            "entrypoint": task.name,
+            "templates": [
+                {
+                    "name": task.name,
+                    "container": {
+                        "image": task.executors[0].image,
+                        "command": task.executors[0].command,
+                        "args": task.executors[0].args,
+                        "resources": {
+                            "limits": {
+                                "cpu": str(task.resources.cpu_cores),
+                                "memory": f"{task.resources.ram_gb}Gi",
+                            }
+                        },
+                    },
+                }
+            ],
+        },
+    }
+
+    return argo_workflow
