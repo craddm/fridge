1e2914e7af2cf1d72cadb53c1a51576d9cc9e379
Matt Craddock <5796417+craddm@users.noreply.github.com>
2026-02-16 10:47:00 +0000

    Add monitoring component to access cluster

--- infra/fridge/access-cluster/components/__init__.py
+++ infra/fridge/access-cluster/components/__init__.py
@@ -2,5 +2,6 @@ from .cert_manager import CertManager, CertManagerArgs
 from .container_registry import ContainerRegistry, ContainerRegistryArgs
 from .fridge_api_jumpbox import FridgeAPIJumpbox, FridgeAPIJumpboxArgs
 from .ingress import Ingress, IngressArgs
+from .monitoring import Monitoring, MonitoringArgs
 from .network_policies import NetworkPolicies
 from .storage_classes import StorageClasses, StorageClassesArgs
