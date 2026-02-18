3b958fb17c8df9dccf571c8fbc37ce86cc285346
Matt Craddock <5796417+craddm@users.noreply.github.com>
2026-02-18 14:51:29 +0000

    Export internal harbor IP address

--- infra/fridge/access-cluster/__main__.py
+++ infra/fridge/access-cluster/__main__.py
@@ -129,7 +129,7 @@ network_policies = components.NetworkPolicies(
 # Pulumi exports
 pulumi.export("fridge_api_ip_address", config.require("fridge_api_ip_address"))
 pulumi.export("harbor_fqdn", harbor.harbor_fqdn)
-pulumi.export("harbor_ip_address", harbor.harbor_lb_ip)
+pulumi.export("harbor_ip_address", harbor.harbor_ip)
 if k8s_environment == K8sEnvironment.AKS:
     pulumi.export("ingress_ip", ingress_nginx.ingress_ip)
     pulumi.export("ingress_ports", ingress_nginx.ingress_ports)
