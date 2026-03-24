0473f6960cf577deb2a4aea1d58e21711a53258c
Matt Craddock <5796417+craddm@users.noreply.github.com>
2026-03-24 15:25:40 +0000

    add init for k8s_api_jumpbox

--- infra/fridge/access-cluster/components/__init__.py
+++ infra/fridge/access-cluster/components/__init__.py
@@ -1,6 +1,7 @@
 from .cert_manager import CertManager, CertManagerArgs
 from .container_registry import ContainerRegistry, ContainerRegistryArgs
 from .fridge_api_jumpbox import FridgeAPIJumpbox, FridgeAPIJumpboxArgs
+from .k8s_api_jumpbox import K8sAPIJumpbox, K8sAPIJumpboxArgs
 from .ingress import Ingress, IngressArgs
 from .network_policies import NetworkPolicies, NetworkPoliciesArgs
 from .storage_classes import StorageClasses, StorageClassesArgs
